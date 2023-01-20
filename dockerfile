f570b247f8ec561d707fe46190ed373baf9013bc
Matthias Jooß <Jooss.Matthias@gmail.com>
2023-01-20 16:36:46 +0100

    reworked fixed lint errors should work

--- dockerfile
+++ dockerfile
@@ -1,4 +1,4 @@
-FROM golang:1.19
+FROM golang:1.19-alpine
 
 WORKDIR /SE_MIM22_WEBSHOP_ORDERSERVICE
 
@@ -8,7 +8,7 @@ RUN go get -d -v ./...
 
 RUN go install -v ./...
 
-EXPOSE 8443
+EXPOSE 8460
 
 RUN go mod download
 
--- main.go
+++ main.go
@@ -7,18 +7,18 @@ import (
 	"time"
 )
 
-func main() { // Server
+func main() {
 	var serveMux = http.NewServeMux()
 	serveMux.HandleFunc("/placeOrder", handler.PlaceOrder)
 	serveMux.HandleFunc("/getOrdersByUserId", handler.GetOrdersByUserID)
-	log.Printf("\n\n\tORDERSERVICE\n\nAbout to listen on Port: 8443." +
+	log.Printf("\n\n\tORDERSERVICE\n\nAbout to listen on Port: 8460." +
 		"\n\nSUPPORTED REQUESTS:" +
 		"\nGET:" +
-		"\nGet Order By ID: http://127.0.0.1:8443/getOrdersByUserId?id=1 requiers a url parameter id" +
+		"\nGet Order By ID: http://127.0.0.1:8460/getOrdersByUserId?id=1 requiers a url parameter id" +
 		"\nPOST:" +
-		"\nPlace Order: http://127.0.0.1:8443/placeOrder requiers a Body with following json:\n{\n    \"produktId\": \"1\",\n    \"userId\": \"1\",\n    \"amount\": \"1\"\n}")
+		"\nPlace Order: http://127.0.0.1:8460/placeOrder requiers a Body with following json:\n{\n    \"produktId\": \"1\",\n    \"userId\": \"1\",\n    \"amount\": \"1\"\n}")
 	server := &http.Server{
-		Addr:              ":8443",
+		Addr:              ":8460",
 		ReadHeaderTimeout: 3 * time.Second,
 		WriteTimeout:      3 * time.Second,
 		IdleTimeout:       3 * time.Second,
